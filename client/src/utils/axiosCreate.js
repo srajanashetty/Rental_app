8931cc224e6dcbe991b344cf5e8458b23e82be5b
Varshashree-R <varshashreegowda21@gmail.com>
2025-10-28 14:54:41 +0530

    Update axiosCreate.js

--- client/src/utils/axiosCreate.js
+++ client/src/utils/axiosCreate.js
@@ -7,7 +7,7 @@ export const injectStore = (_store) => {
 };
 
 const axiosFetch = axios.create({
-  baseURL: https://tenantix.onrender.com,
+  baseURL: import.meta.env.VITE_APP_API_URL || "/api",
   withCredentials: true,
 });
 
